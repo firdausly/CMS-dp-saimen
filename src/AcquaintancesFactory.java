03a30586b9fe856a95d1d89dca2fe39a0466a984
bjunoxx98 <bjunoxx98@gmail.com>
2021-01-08 23:27:48 +0800

    added factory method
    added factory method

--- src/AcquaintancesFactory.java
+++ src/AcquaintancesFactory.java
@@ -1,26 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * ngetes kerol
- * @author alang
- */
-public class AcquaintancesFactory {
-
- public Acquaintances getAcquaintance(int acquaintance){
- switch (acquaintance) {
- case 0:
- return new PersonalFriends();
- case 1:
- return new Relatives();
- case 2:
- return new ProfessionalFriends();
- default:
- return new CasualAcquaintances();
- }
- }
+public abstract class AcquaintancesFactory {
+    
+    public Acquaintances getAcquaintance(int acquaintance){
+        
+        Acquaintances acq;
+        
+        acq = createAcquaintance(acquaintance);
+        
+        return acq;
+    }
+    
+    //create factory method
+    protected abstract Acquaintances createAcquaintance(int acquaintance);
 }
 
