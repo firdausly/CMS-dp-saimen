277634fb120ddb4150c5897740c469c707327596
aiman <aimanshamsudin@yahoo.com>
2021-01-08 23:05:37 +0800

    template pattern

--- src/validDate.java
+++ src/validDate.java
@@ -10,24 +10,29 @@
  */
 import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern; 
+import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
 
-public class validDate extends Validator{
- public validDate(String str) {
- super(str);
+public class validDate extends Validator {
 
-}
- @Override
- public boolean checkValid() {
- String pattern = "[0-3][0-9]/[0-1][0-9]/[0-9]{4}";
- Pattern r = Pattern.compile(pattern);
- Matcher m = r.matcher(str);
- SimpleDateFormat sdf = new
-SimpleDateFormat("dd/MM/yyyy");
- if(!m.find()){
- return false;
- }else
- return true;
+    public validDate(String str) {
+        super(str);
 
-}
+    }
+
+    @Override
+    public boolean checkValid() {
+        String pattern = "[0-3][0-9]/[0-1][0-9]/[0-9]{4}";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(str);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        if (!m.find()) {
+            //add message
+            JOptionPane.showMessageDialog(MUI.getMUI(), "Enter a valid date");
+            return false;
+        } else {
+            return true;
+        }
+
+    }
 }
