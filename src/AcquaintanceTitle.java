422f85ba41d0f67843bbbc285660bb002271b909
firdausly <50550275+firdausly@users.noreply.github.com>
2021-01-11 13:53:17 +0800

    a

--- src/AcquaintanceTitle.java
+++ src/AcquaintanceTitle.java
@@ -1,43 +1,43 @@
 
 import java.util.ArrayList;
 import java.util.Iterator;
+
 public class AcquaintanceTitle extends
-AcquaintanceComponent {
- ArrayList menuComponents = new ArrayList();
- String name;
+        AcquaintanceComponent {
 
- public AcquaintanceTitle(String name) {
- this.name = name;
- }
+    ArrayList menuComponents = new ArrayList();
+    String name;
 
- @Override
- public void add(AcquaintanceComponent
-menuComponent) {
- menuComponents.add(menuComponent);
- }
+    public AcquaintanceTitle(String name) {
+        this.name = name;
+    }
 
- @Override
- public void remove(AcquaintanceComponent
-menuComponent) {
- menuComponents.remove(menuComponent);
- }
+    @Override
+    public void add(AcquaintanceComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
 
- @Override
- public String getName() {
- return name;
- }
+    @Override
+    public void remove(AcquaintanceComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
 
- @Override
- public String print() {
- String s = "";
- Iterator iterator = menuComponents.iterator();
- s = s.concat(getName());
- while (iterator.hasNext()) {
- AcquaintanceComponent menuComponent =
-(AcquaintanceComponent)iterator.next();
- s+=menuComponent.print();
- }
- return s;
+    @Override
+    public String getName() {
+        return name;
+    }
 
- }
+    @Override
+    public String print() {
+        String s = "";
+        Iterator iterator = menuComponents.iterator();
+        s = s.concat(getName());
+        while (iterator.hasNext()) {
+            AcquaintanceComponent menuComponent
+                    = (AcquaintanceComponent) iterator.next();
+            s += menuComponent.print();
+        }
+        return s;
+
+    }
 }
